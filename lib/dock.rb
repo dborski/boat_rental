7f17d4cf6a93c65b5220b0276ed2b35da4b53778
Derek Borski <derek.borski@gmail.com>
2020-03-29 03:43:23 -0600

    Creates return method

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,19 +1,20 @@
 class Dock
 
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :rented_boats
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
-    @rental_log = []
+    @rented_boats = []
+    @total_revenue = 0
   end
 
   def rent(boat, renter)
-    @rental_log << [boat, renter]
+    @rented_boats << [boat, renter]
   end
 
   def rental_log
     categorized_rentals = {}
-    @rental_log.each do |rental|
+    @rented_boats.each do |rental|
       categorized_rentals[rental[0]] = rental[1]
     end
     categorized_rentals
@@ -28,6 +29,27 @@ class Dock
   end
 
   def charge(boat)
-    Hash[card_number: rental_log[boat].credit_card_number, amount: total_charge(boat)]
+    Hash[card_number: rental_log[boat].credit_card_number,
+         amount: total_charge(boat)]
+  end
+
+  def log_hour
+    rental_log.each do |key, value|
+      key.add_hour
+    end
+  end
+
+  def return(boat_parameter)
+    #This method removes boat from rental_log
+    @rented_boats.each do |boat|
+      @rented_boats.delete_at(0) if boat[0] == boat_parameter
+    end
+    @rented_boats
+  end
+
+  def revenue
+    #Revenue only caluclates when all boats are returned
+    #It is a running total of all revenue collected
+    @total_revenue
   end
 end
