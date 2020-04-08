f2ec2b6df5d37b0f8a87f6afba919d767722bdc4
Derek Borski <derek.borski@gmail.com>
2020-04-07 22:00:10 -0600

    Adds return and revnue methods

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,57 +1,41 @@
 class Dock
 
-  attr_reader :name, :max_rental_time, :rented_boats
+  attr_reader :name, :max_rental_time, :rental_log
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
-    @rented_boats = []
+    @rental_log = {}
     @total_revenue = 0
   end
 
   def rent(boat, renter)
-    @rented_boats << [boat, renter]
+    @rental_log[boat] = renter
   end
 
-  def rental_log
-    categorized_rentals = {}
-    @rented_boats.each do |rental|
-      categorized_rentals[rental[0]] = rental[1]
-    end
-    categorized_rentals
-  end
-
-  def total_charge(boat)
-    if boat.hours_rented > max_rental_time
-       max_rental_time * boat.price_per_hour
+  def charge(boat_to_be_charged)
+    boat_we_need = @rental_log.find { |rental| rental[0] = boat_to_be_charged}
+    if boat_we_need[0].hours_rented > max_rental_time
+      {card_number: boat_we_need[1].credit_card_number,
+      amount: max_rental_time * boat_we_need[0].price_per_hour}
     else
-      boat.hours_rented * boat.price_per_hour
+      {card_number: boat_we_need[1].credit_card_number,
+      amount: boat_we_need[0].hours_rented * boat_we_need[0].price_per_hour}
     end
-  end
-
-  def charge(boat)
-    Hash[card_number: rental_log[boat].credit_card_number,
-         amount: total_charge(boat)]
   end
 
   def log_hour
-    rental_log.each do |key, value|
-      key.add_hour
+    rental_log.each do |rental|
+      rental[0].add_hour
     end
   end
 
-  def return(boat_parameter)
-    #This method removes boat from rental_log
-    @rented_boats.each do |boat|
-      @rented_boats.delete_at(0) if boat[0] == boat_parameter
-    end
-    @rented_boats
+  def return(boat_to_be_returned)
+    sum = charge(boat_to_be_returned)[:amount]
+    @rental_log.delete(boat_to_be_returned)
+    @total_revenue += sum
   end
 
   def revenue
-    require "pry"; binding.pry
-
-    #Revenue only caluclates when all boats are returned
-    #It is a running total of all revenue collected
     @total_revenue
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -4,168 +4,144 @@ require './lib/boat'
 require './lib/renter'
 require './lib/dock'
 
-class DockTest < Minitest::Test
-
-  def setup
-    @dock = Dock.new("The Rowing Dock", 3)
-    @kayak_1 = Boat.new(:kayak, 20)
-    @kayak_2 = Boat.new(:kayak, 20)
-    @sup_1 = Boat.new(:standup_paddle_board, 15)
-    @canoe = Boat.new(:canoe, 25)
-    @patrick = Renter.new("Patrick Star", "4242424242424242")
-    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
-  end
+class DockTest < MiniTest::Test
 
   def test_it_exists
+    dock = Dock.new("The Rowing Dock", 3)
 
-    assert_instance_of Dock, @dock
+    assert_instance_of Dock, dock
   end
 
-  def test_it_has_attributes
+  def test_it_has_readable_attributes
+    dock = Dock.new("The Rowing Dock", 3)
 
-    assert_equal "The Rowing Dock", @dock.name
-    assert_equal 3, @dock.max_rental_time
+    assert_equal "The Rowing Dock", dock.name
+    assert_equal 3, dock.max_rental_time
   end
 
-  def test_rental_log
-    @dock.rent(@kayak_1, @patrick)
-    @dock.rent(@kayak_2, @patrick)
-    @dock.rent(@sup_1, @eugene)
+  def test_boats_can_be_rented
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
 
-    assert_equal ({
-       @kayak_1 => @patrick,
-       @kayak_2 => @patrick,
-       @sup_1 => @eugene}), @dock.rental_log
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    expected = {kayak_1 => patrick, kayak_2 => patrick, sup_1 => eugene}
+
+    assert_equal expected, dock.rental_log
   end
 
-  def test_total_charge
-    @dock.rent(@kayak_1, @patrick)
-    @dock.rent(@kayak_2, @patrick)
-    @dock.rent(@sup_1, @eugene)
+  def test_charge_renter
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
 
-    @kayak_1.add_hour
-    @kayak_1.add_hour
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    kayak_1.add_hour
+    kayak_1.add_hour
 
-    assert_equal 40, @dock.total_charge(@kayak_1)
-
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @sup_1.add_hour
-
-    assert_equal 45, @dock.total_charge(@sup_1)
+    expected =  {
+                :card_number => "4242424242424242",
+                :amount => 40
+                }
+    assert_equal expected, dock.charge(kayak_1)
   end
 
-  def test_charge_renter_for_boat
-    @dock.rent(@kayak_1, @patrick)
-    @dock.rent(@kayak_2, @patrick)
-    @dock.rent(@sup_1, @eugene)
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @sup_1.add_hour
-    @dock.charge(@sup_1)
+  def test_cannot_charge_renter_more_than_max_rental_time
+    dock = Dock.new("The Rowing Dock", 3)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
 
-    assert_equal ({
-        :card_number => "1313131313131313",
-        :amount => 45}), @dock.charge(@sup_1)
+    dock.rent(sup_1, eugene)
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
 
-    @kayak_1.add_hour
-    @kayak_1.add_hour
+    expected = {
+                :card_number => "1313131313131313",
+                :amount => 45
+                }
 
-    assert_equal ({
-        :card_number => "4242424242424242",
-        :amount => 40}), @dock.charge(@kayak_1)
+    assert_equal expected, dock.charge(sup_1)
   end
 
-  def test_log_hour_adds_to_hours_rented
-    @dock.rent(@kayak_1, @patrick)
-    @dock.rent(@kayak_2, @patrick)
+  def test_log_hours_increases_every_boats_hours_rented_by_one_hour
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
 
-    @dock.log_hour
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.log_hour
 
-    assert_equal 1, @kayak_1.hours_rented
-    assert_equal 1, @kayak_2.hours_rented
+    assert_equal 1, kayak_1.hours_rented
+    assert_equal 1, kayak_2.hours_rented
 
-    @dock.rent(@canoe, @patrick)
-    @dock.log_hour
+    dock.rent(canoe, patrick)
+    dock.log_hour
 
-    assert_equal 2, @kayak_1.hours_rented
-    assert_equal 2, @kayak_2.hours_rented
-    assert_equal 1, @canoe.hours_rented
+    assert_equal 2, kayak_1.hours_rented
+    assert_equal 2, kayak_2.hours_rented
+    assert_equal 1, canoe.hours_rented
   end
 
-  def test_return_all_boats
-    @dock.rent(@kayak_1, @patrick)
-    @dock.rent(@kayak_2, @patrick)
+  def test_can_return_boats
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
 
-    @dock.log_hour
-    @dock.rent(@canoe, @patrick)
-    @dock.log_hour
+    dock.rent(kayak_1, patrick)
 
-    assert_equal 0, @dock.revenue
+    dock.return(kayak_1)
 
-    @dock.return(@kayak_1)
-    @dock.return(@kayak_2)
-    @dock.return(@canoe)
+    assert_equal ({}), dock.rental_log
+  end
 
-    assert_equal [], @dock.rented_boats
+  def test_revenue_is_counted_when_boat_is_returned
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.log_hour
+
+    dock.rent(canoe, patrick)
+    dock.log_hour
+
+    assert_equal 0, dock.revenue
+
+    dock.return(kayak_1)
+    dock.return(kayak_2)
+    dock.return(canoe)
+
     assert_equal 105, dock.revenue
+
+    dock.rent(sup_1, eugene)
+    dock.rent(sup_2, eugene)
+    dock.log_hour
+    dock.log_hour
+    dock.log_hour
+    dock.log_hour
+    dock.log_hour
+    dock.return(sup_1)
+    dock.return(sup_2)
+
+    assert_equal 195, dock.revenue
   end
 end
-
-## Iteration 4
-
-# Use TDD to update your `Dock` class with the following methods:
-#
-# * `return` - This method takes a `Boat` object as an argument.
-#       When this method is called, it signifies that a boat has been returned and is no longer being rented.
-# * `log_hour` - This method takes no arguments. When it is called,
-#       all boats that are currently rented have been rented an additional hour.
-# * `revenue` - This method takes no arguments.
-#        It returns the total revenue generated by charging all boats that have been rented and returned.
-        # A charge for a boat follows the same rules as iteration 3.
-#
-# The `Dock` class should now respond to the following interaction pattern:
-
-
-#
-# # Revenue should not be generated until boats are returned
-# dock.revenue
-# # => 0
-#
-# dock.return(kayak_1)
-#
-# dock.return(kayak_2)
-#
-# dock.return(canoe)
-#
-# # Revenue thus far
-# dock.revenue
-# # => 105
-#
-# # Rent Boats out to a second Renter
-# dock.rent(sup_1, eugene)
-#
-# dock.rent(sup_2, eugene)
-#
-# dock.log_hour
-#
-# dock.log_hour
-#
-# dock.log_hour
-#
-# # Any hours rented past the max rental time don't factor into revenue
-# dock.log_hour
-#
-# dock.log_hour
-#
-# dock.return(sup_1)
-#
-# dock.return(sup_2)
-#
-# # Total revenue
-# dock.revenue
-# # => 195
